498ae03b1340c3d0a0fa02784e665dd1d063347d
gokhancuryan <119778787+gokhancuryan@users.noreply.github.com>
2022-12-27 20:40:23 +0200

    Add files via upload

---
+++ WhileForeach/ConsoleApp2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi = int.Parse(Console.ReadLine());
+            int sayac = 1;
+            int toplam = 0;
+            while (sayac <= sayi)
+            {
+                toplam += sayac;
+                sayac++;
+            }
+            Console.WriteLine(toplam / sayi);
+
+            // a'dan z'ye kadar olan tüm harfleri konsola yazdır.
+            char character = 'a';
+            while (character < 'z')
+            {
+                Console.Write(character);
+                character++;
+            }
+            string[] arabalar = { "Audi", "Bmw", "mercedes", "toyota", "nissan", "alfa romeo", "ssangyoung" };
+   
+            foreach (var araba in arabalar)
+            {
+                Console.WriteLine(araba);
+            }
+            Console.ReadLine();
+        }
+    }
+}
